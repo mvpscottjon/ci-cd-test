9f353b15948d9cac6e2d25d87294ae23746948fb
Seven Tsai <mvpscottjon@gmail.com>
2023-10-11 01:44:42 +0800

    [Chore] project init

---
+++ ci-cd-test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ci-cd-test
+//
+//  Created by Seven on 2023/10/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
